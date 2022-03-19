80dda34a0a19e2cdbdf4103b590b8dec6ce75c4f
bjornkok <1007695@hr.nl>
2022-03-19 22:57:48 +0100

    Added Utils class
    - diceRoll method
    - updated UML accordingly

--- src/App.java
+++ src/App.java
@@ -2,9 +2,18 @@
 
 //TODO:
 // - Finish the methods in the "Class.java" file
-// - Add a rollDice() method
 // - Add a doesHit() method
 // - Improvements to the way class works?
+
+class Utils {
+    public static int diceRoll(int amount, int type) {
+        int total = 0;
+        for (int i = 0; i < amount; i++) {
+            total += (int) (Math.random() * type) + 1; // It is +1 because the dice rolls from 1 to type, this works! 
+        }
+        return total;
+    }
+}
 
 public class App {
     public static void main(String[] args) throws Exception {
